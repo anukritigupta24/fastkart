1fdde58eab7db7b4a530873d2366a9451c15e671
anukritigupta24 <anukratigupta2@gmail.com>
2024-03-06 01:07:49 +0530

    Added ProductsStore observables and actions

--- src/stores/ProductsStore.ts
+++ src/stores/ProductsStore.ts
@@ -1,17 +1,37 @@
 import { makeObservable, observable, action } from 'mobx';
+import { PRODUCT_URL } from '../constants';
+import { Product } from '../types';
 
 class ProductsStore {
-  products = [];
+  products: Array<Product> = [];
+  limit: number = 10;
+  skip: number = 0;
 
   constructor() {
     makeObservable(this, {
         products: observable,
-        fetchProducts: action
+        skip: observable,
+        addProducts: action,
+        incrementSkip: action,
     });
   }
 
-  fetchProducts = () => {
-    // this.products.push(todo);
+  incrementSkip = () => {
+    this.skip += this.limit;
+  }
+
+  addProducts = (products: Array<Product>) => {
+    this.products = this.products.concat(products);
+  }
+  
+  fetchProducts = async () => {
+    const url = new URL(PRODUCT_URL);
+    url.searchParams.set('limit', this.limit.toString());
+    url.searchParams.set('skip', this.skip.toString());
+
+    const res = await fetch(url);
+    const jsonResponse = await res.json();
+    this.addProducts(jsonResponse?.products);
   };
 }
 
